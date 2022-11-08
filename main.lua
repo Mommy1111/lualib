89bfa6b8a00ee479ef033e5f976946928e4a0997
Mommy1111 <117777211+Mommy1111@users.noreply.github.com>
2022-11-08 21:47:43 +0100

    Create main.lua
    roblox luas liblary to make coding easier !

---
+++ main.lua
@@ -0,0 +1,50 @@
+function weld(main_part,weld_to,position,orientation)
+	local new = Instance.new('Weld',main_part)
+	new.Part0 = new.Parent
+	new.Part1 = weld_to
+	new.Position = Vector3.new(position)
+	new.Orientation = Vector3.new(orientation)
+end
+function prompt_gamepass(id,plr,txt_color,txt_sucess,txt_owned,txt_cancelled)
+	local service = game:GetService('MarketplaceService')
+	local gui = Instance.new('ScreenGui',plr.PlayerGui)
+	gui.Name = 'purchase'
+	local textlabel = Instance.new('TextLabel',gui)
+	textlabel.Position = UDim2.new(0.5,0,0.5,0)
+	textlabel.AnchorPoint = Vector2.new(0.5,0.5)
+	textlabel.TextScaled = true
+	textlabel.BackgroundTransparency = 1
+	textlabel.TextColor3 = Color3.new(tonumber(txt_color))
+	textlabel.Visible = false
+	textlabel.Size = UDim2.new(0.265, 0,0.055, 0)
+	local blur = Instance.new('BlurEffect',game:GetService('Lighting'))
+	if service:UserOwnsGamePassAsync(plr.UserId,id) then
+		textlabel.Text = txt_owned
+		textlabel.Visible = true
+		wait(5)
+		blur:Destroy()
+        textlabel.Visible = false
+		return false
+	else
+		service:PromptGamePassPurchase(plr,id)
+		service.PromptGamePassPurchaseFinished:Connect(function(player,passid,suces)
+			if player.UserId == plr.UserId and passid == id then
+				if suces == true then
+					textlabel.Text = txt_sucess
+					textlabel.Visible = true
+					wait(5)
+					blur:Destroy()
+                    textlabel.Visible = false
+					return true
+				else
+					textlabel.Text = txt_cancelled
+					textlabel.Visible = true
+					wait(5)
+					blur:Destroy()
+                    textlabel.Visible = false
+					return false
+				end
+			end
+		end)
+	end
+end
